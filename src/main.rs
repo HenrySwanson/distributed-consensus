3973161c277347b6dcbf1c522b7b5dcbf89cd42d
Henry Swanson <henryswanson94@gmail.com>
2025-03-08 22:57:24 -0800

    Pull everything into Simulation class

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use std::collections::VecDeque;
 use std::fmt::Display;
 
 use itertools::Itertools;
+use rand::rngs::StdRng;
 use rand::seq::IteratorRandom;
 use rand::Rng;
+use rand::SeedableRng;
 
 const F: usize = 2;
 const N: usize = 2 * F + 1;
@@ -14,75 +16,33 @@ const LOSS_PROBABILITY: f64 = 0.1;
 const DELAY_PROBABILITY: f64 = 0.2;
 const ENABLE_NACKS: bool = true;
 
+// TODO:
+// - Some kind of log that isn't stdout
+// - Deterministic replay
+//   - Could start over from the beginning, or rewind to snapshot and replay.
+//   - Latter seems harder, but we could do it with something like StdRng that can
+//     be cloned.
+// - Timeouts
+//   - Introduce global clock first, then allow for skew later
+//   - For rewind-and-replay, use StdRng so we can clone it as part of the saved state
+// - Network
+//   - Implement message duplication
+//   - Do we distinguish UDP-like and TCP-like messages? (requires timeout/failure/retry)
+// - Other
+//   - Should we use async to implement these?
+//   - Implement crashing!
+
 fn main() {
     println!("Hello, world!");
 
-    let mut processes: Vec<_> = (0..N).map(|i| Process::new(ProcessID(i))).collect();
-    // TODO: introduce message delay, loss, duplication, reordering, etc
-    let mut network = Network::new();
-    let mut rng = rand::rng();
+    let mut sim = Simulation::new();
+    sim.run();
+}
 
-    for round_number in 0..MAX_ROUNDS {
-        println!("==== ROUND {:04} ====", round_number);
-        println!("{} messages pending...", network.len());
-
-        // With low probability, drop a message
-        if !network.is_empty() && rng.random_bool(LOSS_PROBABILITY) {
-            network.drop();
-        }
-        // With slightly higher probability, delay a message
-        if network.len() >= 2 && rng.random_bool(DELAY_PROBABILITY) {
-            network.delay();
-        }
-
-        // If there's nothing in the queue, or with some probability, take a random process
-        // and cause it to issue a proposal. Processes that have decided a value are exempt.
-        if network.is_empty() || rng.random_bool(0.05) {
-            let random_undecided_process = processes
-                .iter_mut()
-                .filter(|p| p.decided_value.is_none())
-                .choose(&mut rng);
-
-            match random_undecided_process {
-                Some(p) => {
-                    println!("Random proposal from {}!", p.id.0);
-                    let proposal_msgs = p.create_proposal_messages();
-                    network.enqueue(proposal_msgs);
-                }
-                None => {
-                    println!("Everyone has decided on a value!");
-                    break;
-                }
-            }
-        } else {
-            let msg = network.next_msg().unwrap();
-            let replies = processes[msg.to.0].recv_message(msg);
-            network.enqueue(replies);
-        };
-
-        // Print current status
-        for p in &processes {
-            println!("{}", p.status());
-        }
-        println!("====================");
-    }
-
-    println!("======== END OF SIMULATION ========");
-    for p in &processes {
-        println!(
-            "Process {} has decided on value {}",
-            p.id.0,
-            p.decided_value.as_ref().map_or("NONE", |s| s.as_str())
-        )
-    }
-    if processes
-        .iter()
-        .all(|p| p.decided_value == processes[0].decided_value)
-    {
-        println!("SUCCESS!");
-    } else {
-        println!("FAILURE!");
-    }
+struct Simulation {
+    processes: [Process; N],
+    network: Network,
+    rng: StdRng,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -120,6 +80,113 @@ enum Message {
     Accept(usize, String),
     Accepted(ProposalID, String), // is value needed?
     Nack(ProposalID),
+}
+
+impl Simulation {
+    fn new() -> Self {
+        Self {
+            processes: std::array::from_fn(|id| Process::new(ProcessID(id))),
+            network: Network::new(),
+            rng: StdRng::from_os_rng(),
+        }
+    }
+
+    fn run(&mut self) {
+        for round_number in 0..MAX_ROUNDS {
+            println!("==== ROUND {:04} ====", round_number);
+            println!("{} messages pending...", self.network.len());
+
+            // With low probability, drop a message
+            if !self.network.is_empty() && self.rng.random_bool(LOSS_PROBABILITY) {
+                self.network.drop();
+            }
+            // With slightly higher probability, delay a message
+            if self.network.len() >= 2 && self.rng.random_bool(DELAY_PROBABILITY) {
+                self.network.delay();
+            }
+
+            // If there's nothing in the queue, or with some probability, take a random process
+            // and cause it to issue a proposal. Processes that have decided a value are exempt.
+            if self.network.is_empty() || self.rng.random_bool(0.05) {
+                let random_undecided_process = self
+                    .processes
+                    .iter_mut()
+                    .filter(|p| p.decided_value.is_none())
+                    .choose(&mut self.rng);
+
+                match random_undecided_process {
+                    Some(p) => {
+                        println!("Random proposal from {}!", p.id.0);
+                        let proposal_msgs = p.create_proposal_messages();
+                        self.network.enqueue(proposal_msgs);
+                    }
+                    None => {
+                        println!("Everyone has decided on a value!");
+                        break;
+                    }
+                }
+            } else {
+                let msg = self.network.next_msg().unwrap();
+                let replies = self.processes[msg.to.0].recv_message(msg);
+                self.network.enqueue(replies);
+            };
+
+            // Print current status
+            for p in &self.processes {
+                println!("{}", p.status());
+            }
+            println!("====================");
+        }
+
+        println!("======== END OF SIMULATION ========");
+        for p in &self.processes {
+            println!(
+                "Process {} has decided on value {}",
+                p.id.0,
+                p.decided_value.as_ref().map_or("NONE", |s| s.as_str())
+            )
+        }
+        if self
+            .processes
+            .iter()
+            .all(|p| p.decided_value == self.processes[0].decided_value)
+        {
+            println!("SUCCESS!");
+        } else {
+            println!("FAILURE!");
+        }
+    }
+
+    fn step(&mut self) {
+        let Some(msg) = self.network.next_msg() else {
+            return;
+        };
+        let replies = self.processes[msg.to.0].recv_message(msg);
+        self.network.enqueue(replies);
+    }
+
+    fn trigger_proposal(&mut self) {
+        let random_undecided_process = self
+            .processes
+            .iter_mut()
+            .filter(|p| p.decided_value.is_none())
+            .choose(&mut self.rng);
+
+        match random_undecided_process {
+            Some(p) => {
+                println!("Random proposal from {}!", p.id.0);
+                let proposal_msgs = p.create_proposal_messages();
+                self.network.enqueue(proposal_msgs);
+            }
+            None => {
+                println!("Everyone has decided on a value!");
+            }
+        }
+    }
+
+    fn network(&mut self) -> &mut Network {
+        &mut self.network
+    }
 }
 
 impl Process {
