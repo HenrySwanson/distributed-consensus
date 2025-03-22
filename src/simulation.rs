5e0dbb3fdd078a2b21e2a5e2650788aa8de18402
Henry Swanson <henryswanson94@gmail.com>
2025-03-22 08:09:20 -0700

    Split apart tick() and run() for Simulation

--- src/simulation.rs
+++ src/simulation.rs
@@ -1,3 +1,5 @@
+use std::ops::ControlFlow;
+
 use itertools::Itertools;
 use network::NetworkSettings;
 use rand::distr::Uniform;
@@ -27,7 +29,7 @@ pub use process::ProcessID;
 
 pub struct Simulation<P: Process> {
     clock: u64,
-    processes: [P; N],
+    state: [ProcessState<P>; N],
     network: Network<P::Message>,
     rng: StdRng,
 }
@@ -50,12 +52,20 @@ pub struct Context<'sim, M> {
     pub outgoing_messages: &'sim mut Vec<Outgoing<M>>,
 }
 
+struct ProcessState<P> {
+    process: P,
+    is_down: bool,
+}
+
 impl<P: Process> Simulation<P> {
     pub fn from_seed(seed: u64) -> Self {
         let mut rng = StdRng::seed_from_u64(seed);
         Self {
             clock: 0,
-            processes: std::array::from_fn(|id| P::new(ProcessID(id))),
+            state: std::array::from_fn(|id| ProcessState {
+                process: P::new(ProcessID(id)),
+                is_down: false,
+            }),
             network: Network::new(
                 // TODO: okay to use StdRng for parent and child?
                 StdRng::from_rng(&mut rng),
@@ -74,67 +84,81 @@ impl<P: Process> Simulation<P> {
         }
     }
 
-    pub fn run(&mut self) -> Consensus {
-        let mut is_down: [_; N] = std::array::from_fn(|_| false);
+    pub fn tick(&mut self) -> ControlFlow<(), ()> {
+        self.clock += 1;
+        log::trace!("==== TICK {:04} ====", self.clock);
+        log::trace!("{} messages pending...", self.network.len());
 
-        for _ in 0..MAX_TICKS {
-            self.clock += 1;
-            log::trace!("==== TICK {:04} ====", self.clock);
-            log::trace!("{} messages pending...", self.network.len());
+        // Are we done?
+        if self
+            .state
+            .iter()
+            .all(|p| p.process.decided_value().is_some())
+        {
+            log::trace!("Everyone has decided on a value!");
+            return ControlFlow::Break(());
+        }
 
-            // Are we done?
-            if self.processes.iter().all(|p| p.decided_value().is_some()) {
-                log::trace!("Everyone has decided on a value!");
-                break;
-            }
-
-            // Randomly crash and uncrash
-            for idx in 0..N {
-                let down = is_down[idx];
-                // never bring down more than a quorum!
-                if !down && self.rng.random_bool(CRASH_PROBABILITY) {
-                    if is_down.iter().filter(|x| **x).count() < F {
-                        log::trace!("Process {} is crashing!", idx);
-                        self.processes[idx].crash();
-                        is_down[idx] = true;
-                    }
-                } else if down && self.rng.random_bool(UNCRASH_PROBABILITY) {
-                    log::trace!("Process {} is back up!", idx);
-                    is_down[idx] = false;
+        // Randomly crash and uncrash
+        for idx in 0..self.state.len() {
+            let down = self.state[idx].is_down;
+            // never bring down more than a quorum!
+            if !down && self.rng.random_bool(CRASH_PROBABILITY) {
+                if self.state.iter().filter(|p| p.is_down).count() < F {
+                    log::trace!("Process {} is crashing!", idx);
+                    self.state[idx].process.crash();
+                    self.state[idx].is_down = true;
                 }
+            } else if down && self.rng.random_bool(UNCRASH_PROBABILITY) {
+                log::trace!("Process {} is back up!", idx);
+                self.state[idx].is_down = false;
             }
+        }
 
-            // Fetch messages
-            let mut msgs_to_deliver: [_; N] = std::array::from_fn(|_| vec![]);
-            while let Some((msg, to)) = self.network.next_msg(self.clock) {
-                msgs_to_deliver[to.0].push(msg);
-            }
+        // Fetch messages
+        let mut msgs_to_deliver: [_; N] = std::array::from_fn(|_| vec![]);
+        while let Some((msg, to)) = self.network.next_msg(self.clock) {
+            msgs_to_deliver[to.0].push(msg);
+        }
 
-            // Tick each process
-            for (idx, messages) in msgs_to_deliver.into_iter().enumerate() {
-                let mut replies = vec![];
-                self.processes[idx].tick(Context {
-                    current_tick: self.clock,
-                    rng: &mut self.rng,
-                    received_messages: messages,
-                    outgoing_messages: &mut replies,
-                });
-                self.network.enqueue(self.clock, ProcessID(idx), replies);
-            }
+        // Tick each process
+        for (idx, messages) in msgs_to_deliver.into_iter().enumerate() {
+            let mut replies = vec![];
+            self.state[idx].process.tick(Context {
+                current_tick: self.clock,
+                rng: &mut self.rng,
+                received_messages: messages,
+                outgoing_messages: &mut replies,
+            });
+            self.network.enqueue(self.clock, ProcessID(idx), replies);
+        }
 
-            // Print current status
-            for p in &self.processes {
-                log::trace!("{}", p.status());
+        // Print current status
+        for p in &self.state {
+            log::trace!("{}", p.process.status());
+        }
+        log::trace!("====================");
+
+        ControlFlow::Continue(())
+    }
+
+    pub fn run(&mut self) -> Consensus {
+        for _ in 0..MAX_TICKS {
+            match self.tick() {
+                ControlFlow::Continue(_) => {}
+                ControlFlow::Break(_) => break,
             }
-            log::trace!("====================");
         }
 
         log::trace!("======== END OF SIMULATION ========");
-        for (i, p) in self.processes.iter().enumerate() {
+        for (i, p) in self.state.iter().enumerate() {
             log::trace!(
                 "Process {} has decided on value {}",
                 i,
-                p.decided_value().as_ref().map_or("NONE", |s| s.as_str())
+                p.process
+                    .decided_value()
+                    .as_ref()
+                    .map_or("NONE", |s| s.as_str())
             )
         }
 
@@ -142,14 +166,18 @@ impl<P: Process> Simulation<P> {
         // for some to have decided on a value and others not, but we can't
         // have two processes deciding on two different values.
         match self
-            .processes
+            .state
             .iter()
-            .filter_map(|p| p.decided_value())
+            .filter_map(|p| p.process.decided_value())
             .all_equal_value()
         {
             Ok(_) => {
                 // could be Complete or Partial
-                if self.processes.iter().all(|p| p.decided_value().is_some()) {
+                if self
+                    .state
+                    .iter()
+                    .all(|p| p.process.decided_value().is_some())
+                {
                     Consensus::Complete
                 } else {
                     Consensus::Partial
