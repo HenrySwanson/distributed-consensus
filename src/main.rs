e9c999b76283480dfed41dd2f96e97acfa4c41e5
Henry Swanson <henryswanson94@gmail.com>
2025-03-29 18:53:04 -0700

    Allow user to run different algorithms from the CLI
    Seems nicer than having to edit it manually :)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+use std::panic::UnwindSafe;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering;
 use std::sync::Arc;
 
 use clap::Parser;
 use paxos::MultiPaxos;
+use paxos::NaiveMultiPaxos;
+use paxos::Paxos;
 use simulation::Consensus;
+use simulation::Process;
 use simulation::Stats;
 
 mod paxos;
@@ -40,6 +44,15 @@ struct Args {
     /// Runs repeatedly until failure
     #[arg(long)]
     stress: bool,
+    #[arg(long)]
+    algorithm: Option<Algorithm>,
+}
+
+#[derive(Debug, Clone, clap::ValueEnum)]
+enum Algorithm {
+    SingleDecree,
+    NaiveMulti,
+    Multi,
 }
 
 fn main() {
@@ -56,6 +69,104 @@ fn main() {
         .init()
         .unwrap();
 
+    let algorithm = args.algorithm.unwrap_or(Algorithm::Multi);
+
+    if args.stress {
+        match algorithm {
+            Algorithm::SingleDecree => stress_test::<Paxos>(),
+            Algorithm::NaiveMulti => stress_test::<NaiveMultiPaxos>(),
+            Algorithm::Multi => stress_test::<MultiPaxos>(),
+        }
+    } else {
+        let seed = args.seed.unwrap_or_else(rand::random);
+        match algorithm {
+            Algorithm::SingleDecree => run_once::<Paxos>(seed),
+            Algorithm::NaiveMulti => run_once::<NaiveMultiPaxos>(seed),
+            Algorithm::Multi => run_once::<MultiPaxos>(seed),
+        }
+    }
+}
+
+fn stress_test<P>()
+where
+    P: Process + UnwindSafe,
+    P::Message: UnwindSafe,
+{
+    let stop_signal = configure_ctrlc_handler();
+
+    let mut successes = 0;
+    let mut incompletes = 0;
+    let mut failures = 0;
+
+    let mut total_stats = Stats::new();
+
+    for n in 0..u64::MAX {
+        let seed = rand::random();
+
+        let mut sim = simulation::Simulation::<P>::from_seed(seed);
+        let (consensus, stats) = match std::panic::catch_unwind(move || {
+            let consensus = sim.run();
+            (consensus, sim.stats())
+        }) {
+            Ok(x) => x,
+            Err(panic) => {
+                // log the failing seed and then resume unwinding
+                log::info!("Seed was: {seed}");
+                std::panic::resume_unwind(panic)
+            }
+        };
+
+        total_stats.merge(stats);
+
+        match consensus {
+            Consensus::None | Consensus::Partial => {
+                log::warn!("Simulation {n} did not complete for seed {seed}");
+                incompletes += 1;
+            }
+            Consensus::Complete => {
+                // log::info!("Simulation {n} did succeeded for seed {seed}");
+                successes += 1;
+            }
+            Consensus::Conflict => {
+                log::error!("Simulation {n} did not succeed for seed {seed}");
+                failures += 1;
+            }
+        }
+
+        if stop_signal.load(Ordering::Relaxed) {
+            break;
+        }
+    }
+
+    println!(
+        "Stress test completed: {} succeeded, {} incomplete, {} failed",
+        successes, incompletes, failures
+    );
+    let total = (successes + incompletes + failures) as f64;
+    println!(
+        "Statistics: {:.2} average ticks to completion, {:.2} average messages sent",
+        total_stats.ticks_elapsed as f64 / total,
+        total_stats.num_messages_sent as f64 / total
+    )
+}
+
+fn run_once<P>(seed: u64)
+where
+    P: Process,
+{
+    let mut sim = simulation::Simulation::<P>::from_seed(seed);
+    let consensus = sim.run();
+
+    log::info!("Seed was: {seed}");
+    if let Consensus::Complete = consensus {
+    } else {
+        log::error!("Simulation did not succeed!");
+    }
+
+    println!("{:?}", sim.stats());
+}
+
+fn configure_ctrlc_handler() -> Arc<AtomicBool> {
     let stop_signal = Arc::new(AtomicBool::new(false));
 
     ctrlc::set_handler({
@@ -69,73 +180,5 @@ fn main() {
         }
     })
     .expect("Error setting Ctrl-C handler");
-
-    if args.stress {
-        let mut successes = 0;
-        let mut incompletes = 0;
-        let mut failures = 0;
-
-        let mut total_stats = Stats::new();
-
-        for n in 0..u64::MAX {
-            let seed = rand::random();
-
-            let mut sim = simulation::Simulation::<MultiPaxos>::from_seed(seed);
-            let (consensus, stats) = match std::panic::catch_unwind(move || {
-                let consensus = sim.run();
-                (consensus, sim.stats())
-            }) {
-                Ok(x) => x,
-                Err(panic) => {
-                    // log the failing seed and then resume unwinding
-                    log::info!("Seed was: {seed}");
-                    std::panic::resume_unwind(panic)
-                }
-            };
-
-            total_stats.merge(stats);
-
-            match consensus {
-                Consensus::None | Consensus::Partial => {
-                    log::warn!("Simulation {n} did not complete for seed {seed}");
-                    incompletes += 1;
-                }
-                Consensus::Complete => {
-                    // log::info!("Simulation {n} did succeeded for seed {seed}");
-                    successes += 1;
-                }
-                Consensus::Conflict => {
-                    log::error!("Simulation {n} did not succeed for seed {seed}");
-                    failures += 1;
-                }
-            }
-
-            if stop_signal.load(Ordering::Relaxed) {
-                break;
-            }
-        }
-
-        println!(
-            "Stress test completed: {} succeeded, {} incomplete, {} failed",
-            successes, incompletes, failures
-        );
-        let total = (successes + incompletes + failures) as f64;
-        println!(
-            "Statistics: {:.2} average ticks to completion, {:.2} average messages sent",
-            total_stats.ticks_elapsed as f64 / total,
-            total_stats.num_messages_sent as f64 / total
-        )
-    } else {
-        let seed = args.seed.unwrap_or_else(rand::random);
-        let mut sim = simulation::Simulation::<MultiPaxos>::from_seed(seed);
-        let consensus = sim.run();
-
-        log::info!("Seed was: {seed}");
-        if let Consensus::Complete = consensus {
-        } else {
-            log::error!("Simulation did not succeed!");
-        }
-
-        println!("{:?}", sim.stats());
-    }
+    stop_signal
 }
